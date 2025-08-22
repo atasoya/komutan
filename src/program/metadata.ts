d430d9aec81420d29cfde097b19dd15413550390
atasoya <ataatasoy2013@gmail.com>
2025-08-22 17:03:13 +0300

    fix metadata

--- src/program/metadata.ts
+++ src/program/metadata.ts
@@ -1,5 +1,5 @@
 import { program } from "./program";
 
-program.name('commando')
-  .description('Commander CLI tool Scaffolding Tool')
+program.name('komutan')
+  .description('A CLI tool for creating and managing CommanderJS CLI tools inspired by cobra-cli')
   .version('1.0.0');
